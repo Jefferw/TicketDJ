4c88be08880adf705afae35cd75ade8c949964ae
lsands-washburn <157529799+lsands-washburn@users.noreply.github.com>
2024-02-12 12:24:27 -0600

    login function progress

--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,30 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User 
+from django.contrib import messages
 # Create your views here.
 def home(request):
     return render(request,"Authentication/index.html")
 
 def signup(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        phoneNumber = request.POST.get('phoneNumber')
+        firstName = request.POST.get('firstName')
+        lastName = request.POST.get('lastName')
+        password = request.POST.get('password')
+
+        newUser = User.objects.create(firstName+" "+lastName, email, password)
+        newUser.phone_number = phoneNumber
+        newUser.first_name = firstName
+        newUser.last_name = lastName
+
+        newUser.save()
+
+        messages.success(request, "Your account has been created successfully.")
+
+        return redirect('signup')
+
     return render(request, "Authentication/signup.html")
 
 def signin(request):
