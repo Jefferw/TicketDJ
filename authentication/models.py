3ebe97800aed996d3f9866eabae02354c60c3307
Jefferw <jefferson.williams@washburn.edu>
2024-02-19 08:12:16 -0600

    Update models.py
    added helpdesk_user to models

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Helpdesk_User(models.Model):
+    email=models.CharField(max_length=255)
+    ephone_number=models.CharField(max_length=255)
+    password=models.CharField(max_length=255)
+    first_name=models.CharField(max_length=255)
+    last_name=models.CharField(max_length=255)
+    user_status=models.CharField(max_length=255)
+    affiliation=models.CharField(max_length=255)
